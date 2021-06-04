7e589dbc5e88d66aa50cab1a90c400b1025e55c1
rbgreenway3rd <3rdgreenway@gmail.com>
2021-06-04 14:19:59 -0500

    began implementation of AnimalDetail module. Not functional yet.

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,35 +1,68 @@
-import React, { useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { AnimalContext } from "./AnimalProvider";
+import { Animal } from "./Animal";
 import "./Animal.css";
 
-export const AnimalList = () => {
-  // This state changes when `getAnimals()` is invoked below
-  const { animals, getAnimals } = useContext(AnimalContext);
+export const AnimalList = ({ history }) => {
+  const { getAnimals, animals } = useContext(AnimalContext);
 
-  //useEffect - reach out to the world for something
+  // Initialization effect hook -> Go get animal data
   useEffect(() => {
-    console.log("AnimalList: useEffect - getAnimals");
     getAnimals();
   }, []);
-  const history = useHistory();
 
   return (
     <>
-      <h2>Animals</h2>
+      <h1>Animals</h1>
+
       <button onClick={() => history.push("/animals/create")}>
-        Add Animal
+        Make Reservation
       </button>
+
       <div className="animals">
-        {animals.map((animal) => {
-          return (
-            <div className="animal" key={animal.id} id={`animal--${animal.id}`}>
-              <div className="animal__name">Name: {animal.name}</div>
-              <div className="animal__breed">Breed: {animal.breed}</div>
-            </div>
-          );
-        })}
+        {animals.map((animal) => (
+          <Link to={`/animals/detail/${animal.id}`} key={animal.id}>
+            {animal.name}
+          </Link>
+        ))}
       </div>
     </>
   );
 };
+
+// import React, { useContext, useEffect } from "react";
+// import { useHistory } from "react-router-dom";
+// import { AnimalContext } from "./AnimalProvider";
+// import "./Animal.css";
+
+// export const AnimalList = () => {
+//   // This state changes when `getAnimals()` is invoked below
+//   const { animals, getAnimals } = useContext(AnimalContext);
+
+//   //useEffect - reach out to the world for something
+//   useEffect(() => {
+//     console.log("AnimalList: useEffect - getAnimals");
+//     getAnimals();
+//   }, []);
+//   const history = useHistory();
+
+//   return (
+//     <>
+//       <h2>Animals</h2>
+//       <button onClick={() => history.push("/animals/create")}>
+//         Add Animal
+//       </button>
+//       <div className="animals">
+//         {animals.map((animal) => {
+//           return (
+//             <div className="animal" key={animal.id} id={`animal--${animal.id}`}>
+//               <div className="animal__name">Name: {animal.name}</div>
+//               <div className="animal__breed">Breed: {animal.breed}</div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </>
+//   );
+// };
